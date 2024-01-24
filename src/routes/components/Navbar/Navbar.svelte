cb725a33f1116ab2f56553e355f21f90773902d7
--global <--global>
2024-01-24 18:28:31 +0700

    add some navbar items

--- src/routes/components/Navbar/Navbar.svelte
+++ src/routes/components/Navbar/Navbar.svelte
@@ -1,1 +1,22 @@
-<nav class="flex h-16 items-center border-b-[1px] px-3">This is nav</nav>
+<script>
+  import logo from '$lib/assets/logo.png';
+  import Search from './Search.svelte';
+  import UserActions from './UserActions.svelte';
+</script>
+
+<nav class="flex h-[70px] items-center justify-between border-b-[1px] px-6 [&>*]:flex-1">
+  <div class="flex items-center gap-4">
+    <a href="/" class="rounded-lg border-[1px] p-1 shadow-md">
+      <div class="w-10">
+        <img class="max-w-full" src="{logo}" alt="logo" />
+      </div>
+    </a>
+    <p class="font-medium">Lưu và đánh giá Anime</p>
+  </div>
+
+  <div class="flex justify-center">
+    <Search />
+  </div>
+
+  <UserActions />
+</nav>
---
+++ src/routes/components/Navbar/Search.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte';
+
+  function onFocus(event: FocusEvent) {
+    (event.target as HTMLInputElement).select();
+  }
+</script>
+
+<div
+  class="flex w-96 flex-row-reverse items-center gap-1 rounded-full border-2 px-2 py-1 transition-colors focus-within:border-slate-800">
+  <input
+    on:focus="{onFocus}"
+    type="text"
+    class="peer w-11/12 text-slate-800"
+    placeholder="Tìm kiếm anime, meme, ..." />
+  <Icon
+    icon="akar-icons:search"
+    class="text-slate-700 transition-colors duration-200 hover:!text-slate-700 peer-placeholder-shown:text-slate-500"
+    width="25"
+    height="25" />
+</div>
